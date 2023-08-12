072882f2611feea361a0400608d45be0c65b4557
MrGalletah <mdcabrera@hotmail.es>
2023-08-12 18:10:38 -0300

    solucion

--- eventos.js
+++ eventos.js
@@ -2,7 +2,12 @@ window.onload = function () {
   const div = document.querySelector("div");
   const button = document.getElementById("button");
 
-  div.addEventListener("click", () => {
+  div.addEventListener("click", function alerta() {
     alert("Hola! Soy el div");
+  });
+
+  button.addEventListener("click", (e) => {
+    e.stopPropagation();
+    alert("Hola!");
   });
 };
